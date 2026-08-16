72aeb602fc4a22f8709e07f919c30e09a6e920be
theultimateremedy84-cyber <theultimateremedy84@gmail.com>
2026-08-16 14:56:14 +0400

    Wipe all files from repository

--- lib/db/src/index.ts
+++
@@ -1,4 +0,0 @@
-// ── Database exports ─────────────────────────────────────────────────────────
-// This package is server-side only. Do NOT import React/browser code here.
-export { db } from "./db";
-export * from "./schema";
